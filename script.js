7318e8d1d21ad8410daa88d5ed2e6c07257b51df
ayushi-sket <ayushin466@gmail.com>
2026-06-05 13:04:49 +0530

    Completed Day 4 JavaScript tasks

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 console.log("JS is connected!");
 
-// Welcome message
+// Welcome Message
 alert("Welcome to my Portfolio!");
 
-// About Section Update
+// About Section Button
 const aboutBtn = document.getElementById("aboutBtn");
+
+console.log(aboutBtn);
 
 aboutBtn.addEventListener("click", function () {
     document.getElementById("aboutText").innerText =
-    "I am learning JavaScript and building interactive websites.";
+        "I am learning JavaScript and building interactive websites.";
 });
 
-// Dark Mode Toggle
+// Dark Mode Button
 const themeBtn = document.getElementById("themeBtn");
 
 themeBtn.addEventListener("click", function () {
     document.body.classList.toggle("dark-mode");
+});
+
+// Select Multiple Elements with Same Class
+const buttons = document.querySelectorAll(".btn");
+
+console.log(buttons);
+
+// Select Heading and Change Text on Click
+const heading = document.querySelector(".hero h1");
+
+heading.addEventListener("click", function () {
+    heading.innerText = "Frontend Developer";
+});
+
+// Skills Section Color Change
+const skillsSection = document.getElementById("skills");
+
+skillsSection.addEventListener("click", function () {
+    skillsSection.style.backgroundColor = "#dbeafe";
 });
